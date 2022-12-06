99ebe6e21f2095f2611a822df471358e7e013751
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2022-12-07 01:36:20 +0800

    lezz go

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # portfolio
  
+Good day here is my web portfolio expressing my creativity and skills in coding
+Give me feedback and
+follow my socials
+
+thank you
+
+
--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const navBarLinks = document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click',() => {
     navBarLinks.classList.toggle('active')
 })
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//     $('.Box').css("opacity", 1- $(window).scrollTop() / 700)
+//     })
+// })
